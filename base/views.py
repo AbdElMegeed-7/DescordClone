8304ffdd8b48186fc56d68c35e0a03c4edd3146d
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2021-12-21 16:34:24 +0200

    Room Model

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Room
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'base/home.html')
+
+def room(request):
+    return render(request, 'base/room.html')
